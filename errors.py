630941acd0b369d7384e732acc36ad7eca211f55
Daref24 <115921424+Daref24@users.noreply.github.com>
2022-12-21 15:42:47 +0200

    Add files via upload

---
+++ errors.py
@@ -0,0 +1,17 @@
+class InvalidCommand(Exception):
+    pass
+
+class InvalidDataFormat(Exception):
+    pass
+
+class CharacterNotFound(Exception):
+    pass
+
+class InvalidCharacterClass(Exception):
+    pass
+
+class CharacterExists(Exception):
+    pass
+
+class InvalidGender(Exception):
+    pass
